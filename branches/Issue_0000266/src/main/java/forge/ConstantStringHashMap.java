0c699aeeda4f5ae33bcc66543c09b56781f0fd3a
ArsenalNut <ArsenalNut@269b9781-a132-4a9b-9d4e-f004f1b56b58>
2011-12-30 20:36:21 +0000

    Creating branch for changes for supporting "Produced$ Any" in mana abilities

---
+++ branches/Issue_0000266/src/main/java/forge/ConstantStringHashMap.java
@@ -0,0 +1,15 @@
+/*package forge;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * The Class Constant_StringHashMap.
+ *
+public class ConstantStringHashMap {
+
+    /** The map. *
+    private Map<String, String> map = new HashMap<String, String>();
+
+}
+ */

24af540e94a2df41baa496930bad0a6fce22f097
Chris H. <Chris H.@269b9781-a132-4a9b-9d4e-f004f1b56b58>
2011-12-30 12:15:41 +0000

    [maven-release-plugin]  copy for tag forge-1.2.0

---
+++ tags/forge-1.2.0/src/test/java/forge/PhaseTest.java
@@ -0,0 +1,22 @@
+package forge;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by IntelliJ IDEA. User: dhudson
+ */
+@Test(groups = { "UnitTest" }, timeOut = 5000, enabled = false)
+public class PhaseTest {
+
+    /**
+     * Phase test1.
+     */
+    @Test(groups = { "UnitTest", "fast" }, timeOut = 5000, enabled = false)
+    public void phaseTest1() {
+        final Phase phase = new Phase();
+        for (int i = 0; i < phase.getPhaseOrder().length; i++) {
+            System.out.println(phase.getPlayerTurn() + " " + phase.getPhase());
+            phase.nextPhase();
+        }
+    }
+}

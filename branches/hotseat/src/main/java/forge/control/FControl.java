836c5e42b51de9ea5e0a9d1303d3f2560df74c95
Max mtg <Max mtg@269b9781-a132-4a9b-9d4e-f004f1b56b58>
2012-10-18 08:13:36 +0000

    changes from 17550-17560 integrated

--- branches/hotseat/src/main/java/forge/control/FControl.java
+++ branches/hotseat/src/main/java/forge/control/FControl.java
@@ -202,7 +202,7 @@ public enum FControl {
             case HOME_SCREEN:
                 VHomeUI.SINGLETON_INSTANCE.populate();
                 CHomeUI.SINGLETON_INSTANCE.initialize();
-                //FView.SINGLETON_INSTANCE.getPnlInsets().setVisible(true);
+                FView.SINGLETON_INSTANCE.getPnlInsets().setVisible(true);
                 FView.SINGLETON_INSTANCE.getPnlInsets().setForegroundImage(new ImageIcon());
                 Singletons.getView().getFrame().addWindowListener(waDefault);
                 break;

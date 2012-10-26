08de3bc536fb050e56cb5e40164ea4c1bac7c20b
Chris H. <Chris H.@269b9781-a132-4a9b-9d4e-f004f1b56b58>
2012-10-26 10:54:36 +0000

    [maven-release-plugin]  copy for tag forge-1.3.0

---
+++ tags/forge-1.3.0/src/test/java/forge/GuiProgressBarWindowTest.java
@@ -0,0 +1,31 @@
+package forge;
+
+import javax.swing.WindowConstants;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import forge.gui.GuiProgressBarWindow;
+
+/**
+ * Created by IntelliJ IDEA. User: dhudson
+ */
+@Test(groups = { "UnitTest" })
+public class GuiProgressBarWindowTest {
+
+    /**
+     * Gui progress bar window test1.
+     */
+    @Test(groups = { "UnitTest", "fast" })
+    public void guiProgressBarWindowTest1() {
+        try {
+            final GuiProgressBarWindow dialog = new GuiProgressBarWindow();
+            dialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+            dialog.setVisible(true);
+            Assert.assertNotNull(dialog);
+            dialog.dispose();
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+    }
+}

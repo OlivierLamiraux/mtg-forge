e3349d8ef28c75f551a3126489af68866cca4fdf
Agetian <Agetian@269b9781-a132-4a9b-9d4e-f004f1b56b58>
2013-03-08 05:39:24 +0000

    - Checkstyle on AIProfile.

--- branches/AIPersonalities/src/main/java/forge/game/ai/AiProfile.java
+++ branches/AIPersonalities/src/main/java/forge/game/ai/AiProfile.java
@@ -17,10 +17,6 @@
  */
 package forge.game.ai;
 
-import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -52,7 +48,7 @@ public class AiProfile {
         private final String strDefaultVal;
 
         /** @param s0 &emsp; {@link java.lang.String} */
-        AIProps(String s0) {
+        AIProps(final String s0) {
             this.strDefaultVal = s0;
         }
 
@@ -64,8 +60,10 @@ public class AiProfile {
 
     /** Builds an AI profile file name with full relative 
      * path based on the profile name. 
+     * @param profileName the name of the profile.
+     * @return the full relative path and file name for the given profile.
      */
-    private String buildFileName(String profileName) {
+    private String buildFileName(final String profileName) {
         return String.format("res/ai/%s.ai", profileName);
     }
     
@@ -81,7 +79,7 @@ public class AiProfile {
      * @param q0 &emsp; {@link forge.properties.ForgePreferences.AIProps}
      * @param s0 &emsp; {@link java.lang.String} value
      */
-    private void setPref(AIProps q0, String s0) {
+    private void setPref(final AIProps q0, final String s0) {
         aiProfileProps.put(q0, s0);
     }
 
@@ -90,7 +88,7 @@ public class AiProfile {
      * @param q0 AIProps
      * @param val boolean
      */
-    private void setPref(AIProps q0, int val) {
+    private void setPref(final AIProps q0, final int val) {
         setPref(q0, String.valueOf(val));
     }
 
@@ -99,12 +97,15 @@ public class AiProfile {
      * @param q0 AIProps
      * @param val boolean
      */
-    private void setPref(AIProps q0, boolean val) {
+    private void setPref(final AIProps q0, final boolean val) {
         setPref(q0, String.valueOf(val));
     }
 
-    /** Set the current AI profile (loads AI properties from file). */
-    public void setProfile(String profileName) {
+    /** 
+     * Set the current AI profile (loads AI properties from file). 
+     * @param profileName the name of the profile to load.
+     */
+    public final void setProfile(final String profileName) {
         reset();
 
         List<String> lines = FileUtil.readFile(buildFileName(profileName));
@@ -130,7 +131,7 @@ public class AiProfile {
      * @param fp0 &emsp; {@link forge.quest.data.ForgePreferences.AIProps}
      * @return String
      */
-    public static String getAIProp(AIProps fp0) {
+    public static String getAIProp(final AIProps fp0) {
         String val;
 
         val = aiProfileProps.get(fp0);
@@ -145,7 +146,7 @@ public class AiProfile {
      * @param fp0 &emsp; {@link forge.quest.data.ForgePreferences.AIProps}
      * @return int
      */
-    public static int getAIPropInt(AIProps fp0) {
+    public static int getAIPropInt(final AIProps fp0) {
         return Integer.parseInt(getAIProp(fp0));
     }
 
@@ -155,15 +156,16 @@ public class AiProfile {
      * @param fp0 &emsp; {@link forge.quest.data.ForgePreferences.AIProps}
      * @return boolean
      */
-    public static boolean getAIPropBoolean(AIProps fp0) {
+    public static boolean getAIPropBoolean(final AIProps fp0) {
         return Boolean.parseBoolean(getAIProp(fp0));
     }
 
     /**
      * Returns the name of the current AI profile.
+     * @return the name of the currently used AI profile, can be empty if
+     * no profile is loaded and default settings are used.
      */
     public static String getCurrentProfileName() {
         return currentProfile;
     }
 }
-

e98872a3640c4277f7ee1c4f8a4079d9f4ef2866
myk <myk@269b9781-a132-4a9b-9d4e-f004f1b56b58>
2013-03-17 05:34:34 +0000

    clean up merged branch

--- branches/myk-separate-writable-data/src/main/java/forge/game/event/DuelOutcomeEvent.java
+++
@@ -1,9 +0,0 @@
-package forge.game.event;
-
-public class DuelOutcomeEvent extends Event {
-    public final boolean humanWonTheDuel;
-
-    public DuelOutcomeEvent(boolean humanWon) {
-        humanWonTheDuel = humanWon;
-    }
-}

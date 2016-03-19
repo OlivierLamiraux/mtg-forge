fab1fefb865e3b76b517ef9a871932cf2cd7fd0f
KrazyTheFox <KrazyTheFox@269b9781-a132-4a9b-9d4e-f004f1b56b58>
2016-03-19 04:51:19 +0000

    [maven-release-plugin]  copy for tag forge-1.5.50

---
+++ tags/forge-1.5.50/forge-gui/src/main/java/forge/planarconquest/ConquestChaosBattle.java
@@ -0,0 +1,129 @@
+package forge.planarconquest;
+
+import java.io.File;
+import java.util.EnumSet;
+import java.util.Set;
+
+import forge.LobbyPlayer;
+import forge.deck.Deck;
+import forge.game.GameType;
+import forge.game.GameView;
+import forge.interfaces.IButton;
+import forge.interfaces.IGuiGame;
+import forge.interfaces.IWinLoseView;
+import forge.item.PaperCard;
+import forge.model.FModel;
+import forge.planarconquest.ConquestPreferences.CQPref;
+import forge.properties.ForgeConstants;
+import forge.quest.QuestEventDifficulty;
+import forge.quest.QuestEventDuel;
+import forge.quest.QuestEventDuelManager;
+import forge.quest.QuestWorld;
+import forge.util.Aggregates;
+
+public class ConquestChaosBattle extends ConquestBattle {
+    private final QuestWorld world;
+    private final QuestEventDuel duel;
+    private ConquestAwardPool awardPool;
+
+    public ConquestChaosBattle() {
+        super(null, 0);
+
+        //use a random quest event duel for Chaos battle
+        //using Chaos battle record to determine difficulty
+        QuestEventDifficulty difficulty;
+        ConquestPreferences prefs = FModel.getConquestPreferences();
+        int chaosBattleWins = FModel.getConquest().getModel().getChaosBattleRecord().getWins();
+        if (chaosBattleWins < prefs.getPrefInt(CQPref.CHAOS_BATTLE_WINS_MEDIUMAI)) {
+            difficulty = QuestEventDifficulty.EASY;
+        }
+        else if (chaosBattleWins < prefs.getPrefInt(CQPref.CHAOS_BATTLE_WINS_HARDAI)) {
+            difficulty = QuestEventDifficulty.MEDIUM;
+        }
+        else if (chaosBattleWins < prefs.getPrefInt(CQPref.CHAOS_BATTLE_WINS_EXPERTAI)) {
+            difficulty = QuestEventDifficulty.HARD;
+        }
+        else {
+            difficulty = QuestEventDifficulty.EXPERT;
+        }
+
+        world = Aggregates.random(FModel.getWorlds());
+        String path = world == null || world.getDuelsDir() == null ? ForgeConstants.DEFAULT_DUELS_DIR : ForgeConstants.QUEST_WORLD_DIR + world.getDuelsDir();
+        QuestEventDuelManager duelManager = new QuestEventDuelManager(new File(path));
+        duel = Aggregates.random(duelManager.getDuels(difficulty));
+    }
+
+    @Override
+    protected Deck buildOpponentDeck() {
+        return duel.getEventDeck();
+    }
+
+    @Override
+    public String getEventName() {
+        return duel.getTitle();
+    }
+
+    @Override
+    public String getOpponentName() {
+        return duel.getName();
+    }
+
+    @Override
+    public PaperCard getPlaneswalker() {
+        return null;
+    }
+
+    @Override
+    public void setOpponentAvatar(LobbyPlayer aiPlayer, IGuiGame gui) {
+        gui.setPlayerAvatar(aiPlayer, duel);
+    }
+
+    @Override
+    public Set<GameType> getVariants() {
+        return EnumSet.noneOf(GameType.class);
+    }
+
+    @Override
+    public int gamesPerMatch() {
+        return 3; //play best 2 out of 3 for Chaos battles
+    }
+
+    @Override
+    public void showGameOutcome(final ConquestData model, final GameView game, final LobbyPlayer humanPlayer, final IWinLoseView<? extends IButton> view) {
+        if (game.isMatchOver()) {
+            view.getBtnContinue().setVisible(false);
+            if (game.isMatchWonBy(humanPlayer)) {
+                view.getBtnQuit().setText("Great!");
+                model.getChaosBattleRecord().addWin();
+                setConquered(true);
+            }
+            else {
+                view.getBtnQuit().setText("OK");
+                model.getChaosBattleRecord().addLoss();
+            }
+            model.saveData();
+        }
+        else {
+            view.getBtnContinue().setVisible(true);
+            view.getBtnContinue().setText("Continue");
+            view.getBtnQuit().setText("Quit");
+        }
+    }
+
+    @Override
+    public void onFinished(final ConquestData model, IWinLoseView<? extends IButton> view) {
+        if (view.getBtnContinue().isVisible()) {
+            //ensure loss saved if you quit the battle before the match is over
+            model.getChaosBattleRecord().addLoss();
+            model.saveData();
+        }
+        super.onFinished(model, view);
+    }
+
+    public ConquestAwardPool getAwardPool() {
+        if (awardPool == null) { //delay initializing until needed
+            awardPool = new ConquestAwardPool(world.getAllCards());
+        }
+        return awardPool;
+    }
+}
